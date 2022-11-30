324e0e4d4656fc35050b23215cdc7900acd2bf71
Jesse <JuicinessJMN@Gmail.com>
2022-11-30 16:23:06 -0600

    rough work needs polishing and refining

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
+/* Quiz game
+1. To start game need to create an listener event.
+2. Once the event is trigger it'll run the startGame function.
+3. Inside the startGame function it should change the display of the screen from the default starter to displaying questions, which should also start the startTimer function.
+4. With 1 or 4 listener events inside each questions asked it'll run a findAnswer function to determine if answer selected was correct or incorrect.
+5. After an answer is selected regardless if correct of incorrect it'll store the variable of correct or incorrect and proceed displaying the next question until all 8 have been displayed.
+6. Which when it'll change screen and trigger the endGame function which will change screen from the questionnaire to results of how many correct over amount of questions ie. x/8.
+6a. If timer was to run out it'll change screen by triggering the endGame function too with amount of questions answered correctly over amount of questions that should've been displayed.
+
+Idea:
+- 
+*/
+
+
+
+// Var elements
 var scoresEl = document.querySelector(".highScores");
 var timerEl = document.querySelector(".timeLeft");
 var questionsEl = document.querySelector(".questions");
@@ -5,34 +21,134 @@ var startBtn = document.querySelector(".gameStart");
 var answerBtn = document.querySelector("#answers");
 
 
+// Arrays 
+var questions = [
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    },
+    {
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        correct: ""
+    }
+];
 
-var questions = [];
-var answers = [];
-var correct = [];
-
-
-function startGame() {
- // starts the game by changing display from into to game. Then triggers the questionaire function  
- questionaire();
-}
+// Functions
 
 function endGame() {
-    //ends game and changes display to display time remaning as points or DNF    
+    questionsEl.textContent = "Game Over!"
+    var scores = theGame();
+    var scoresEl = document.querySelector("#highScores");
+    scoresEl.textContent = scores;
 }
 
-function timerEl() {
-    // timer function
+function timer() {
+    let secondsLeft = 60;
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft + "remaining.";
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            endGame();
+        }
+    }, 1000);
 }
 
-
-function questionaire() {
-    for (var x = 0; x < questionsEl.length; x++)
+function theGame() {
+    timer()
+    for (let x = 0; x < questions.length; x++)
     {
-        answerBtn.addEventListener("click", function answerChecker(){
-            //Figure out how to either use 1 if statement or need to use 4 if statement by recognizing each button option and run inside array to determine if answer pressed was correct or not.
+        questionsEl.textContent = questions.question;
+        document.getElementById("answer1") = questions.textContent.answer1;
+        document.getElementById("answer2") = questions.textContent.answer2;
+        document.getElementById("answer3") = questions.textContent.answer3;
+        document.getElementById("answer4") = questions.textContent.answer4;
+        let correct = 0;
+        answerBtn.addEventListener("click", function(){
+            if (answerBtn === questions.correct)
+            {
+                correct++;
+            }
+            return correct;
         })
     }
 }
 
-startBtn.addEventListener("click", startGame())
 
+startBtn.addEventListener("click", theGame())
